4083769b70a3f3368701312ca984a6460c8c9032
glendc <glen.decauwsemaecker@otainsight.com>
2021-05-15 22:17:20 +0200

    add simple hyper echo server

---
+++ webservice-hyper/README.md
@@ -0,0 +1,4 @@
+# webservice-hyper
+
+Based on the "server" example of the Hyper Starting Guide:
+<https://hyper.rs/guides/server/hello-world/>
---
+++ webservice-hyper/src/main.rs
@@ -0,0 +1,44 @@
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Method, Request, Response, Server, StatusCode};
+use std::convert::Infallible;
+use std::net::SocketAddr;
+
+// helper function to create a service using a common pattern,
+// NOTE the error type (one that can never fail)
+async fn hello_world(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    let mut response = Response::new(Body::empty());
+
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/") => {
+            *response.body_mut() = Body::from("Try POSTing data to /echo");
+        }
+        (&Method::POST, "/echo") => {
+            *response.body_mut() = req.into_body();
+        }
+        _ => {
+            *response.status_mut() = StatusCode::NOT_FOUND;
+        }
+    };
+
+    Ok(response)
+}
+
+#[tokio::main]
+async fn main() {
+    // We'll bind to 127.0.0.1:3000
+    let addr = SocketAddr::from(([127, 0, 0, 1], 7878));
+
+    // A `Service` is needed for every connection, so this
+    // creates one from our `hello_world` function.
+    let make_svc = make_service_fn(|_conn| async {
+        // service_fn converts our function into a `Service`
+        Ok::<_, Infallible>(service_fn(hello_world))
+    });
+
+    let server = Server::bind(&addr).serve(make_svc);
+
+    // Run this server for... forever!
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
+}
